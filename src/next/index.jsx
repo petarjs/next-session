5acc60e380e5c9f17c80f0fe624eea7fc50318f7
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-09 00:55:51 -0500

    Only run if component has getInitialProps

--- src/next/index.jsx
+++ src/next/index.jsx
@@ -2,33 +2,30 @@ import React from 'react';
 import { parse as parseCookie } from 'cookie';
 import nextSession from '../index';
 
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export function withSession(Page, options) {
-  const strategy = options.strategy || 'hybrid';
   const WithSession = props => <Page {...props} />;
-  if (strategy === 'hybrid') {
-    WithSession.getInitialProps = async ctx => {
-      let pageProps = {};
-      if (ctx.req && ctx.res) {
-        ctx.req.cookies =
-          ctx.req.cookies ||
-          (ctx.req.headers &&
-            typeof ctx.req.headers.cookie === 'string' &&
-            parseCookie(ctx.req.headers.cookie)) ||
-          {};
-        await new Promise(resolve => {
-          nextSession(options)(ctx.req, ctx.res, resolve);
-        });
-      }
-      ctx.session = (ctx.req && ctx.req.session) || undefined;
-      if (Page.getInitialProps) {
-        pageProps = await Page.getInitialProps(ctx);
-      }
-      return { session: ctx.session, ...pageProps };
+  WithSession.displayName = `withSession(${getDisplayName(Page)})`;
+  if (Page.getInitialProps)
+    WithSession.getInitialProps = context => {
+      const ctx = context.ctx || context;
+      if (typeof window !== 'undefined') return Page.getInitialProps(ctx);
+      ctx.req.cookies =
+        ctx.req.cookies ||
+        (ctx.req.headers &&
+          typeof ctx.req.headers.cookie === 'string' &&
+          parseCookie(ctx.req.headers.cookie)) ||
+        {};
+      return new Promise(resolve => {
+        nextSession(options)(ctx.req, ctx.res, resolve);
+      }).then(() => {
+        ctx.session = (ctx.req && ctx.req.session) || undefined;
+        return Page.getInitialProps(ctx);
+      });
     };
-  }
-  WithSession.displayName = `withSession(${Page.displayName ||
-    Page.name ||
-    'Component'})`;
   return WithSession;
 }
